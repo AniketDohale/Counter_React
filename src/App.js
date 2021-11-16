bf593b8e01009c457f98ece97f42261df38355cd
AniketDohale <aniket.dohale.2801@gmail.com>
2021-11-16 17:03:36 +0530

    First Stage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counterValue, setCounterValue] = useState(10);
+    const [changeValue, setChangeValue] = useState(1);
+
+    useEffect(() => {
+        fetch("/data.json").then((response) => response.json())
+            .then((data) => {
+                setCounterValue(data.counter);
+                setChangeValue(data.changeValue);
+            })
+            .catch((err) => console.log(err));
+    }, [])
+
+    return (
+        <div className="App">
+            <input value={changeValue} type="number" onChange={(e) => { setChangeValue(parseInt(e.target.value)); }} />
+            <div className="counter">
+                <button onClick={() => setCounterValue((prevValue) => prevValue + changeValue)}>+</button>
+                <p>{counterValue}</p>
+                <button onClick={() => setCounterValue((prevValue) => prevValue - changeValue)}>-</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
